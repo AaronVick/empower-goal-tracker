ecef5e2149f1530d78a830a5ebb0c907ae502012
Aaron <33428648+AaronVick@users.noreply.github.com>
2024-08-24 17:20:17 -0500

    update

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
-/** @type {import('next').NextConfig} */
+// next.config.js
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['empower-goal-tracker.vercel.app'],
+    domains: ['empower-goal-tracker.vercel.app'], // Ensure this domain is listed
+    unoptimized: true, // Disable Next.js image optimization
   },
-}
+  webpack: (config) => {
+    config.externals = [...config.externals, 'sharp'];
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
