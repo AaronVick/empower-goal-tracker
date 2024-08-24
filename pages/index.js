023ffd9d2e5a25e9b42b39eee02b5d4f18c18e07
Aaron <33428648+AaronVick@users.noreply.github.com>
2024-08-24 16:11:43 -0500

    update

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 
+//fucntion home
 export default function Home() {
   const router = useRouter();
   const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
