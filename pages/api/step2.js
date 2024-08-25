e116cd2d7fc36f06bb7ec3c1c80e923b5968fee1
Aaron <33428648+AaronVick@users.noreply.github.com>
2024-08-24 20:18:14 -0500

    update

--- pages/api/step2.js
+++ pages/api/step2.js
@@ -1,9 +1,6 @@
 export default function handler(req, res) {
   console.log('Step 2 API accessed');  // Logging to confirm access to Step 2
   
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_PATH || '';
-  const fullBaseUrl = `https://empower-goal-tracker.vercel.app${baseUrl}`;
-
   res.setHeader('Content-Type', 'text/html');
   res.status(200).send(`
     <!DOCTYPE html>
@@ -13,7 +10,7 @@ export default function handler(req, res) {
       <meta property="fc:frame:image" content="https://empower-goal-tracker.vercel.app/addGoal.png"/>
       <meta property="fc:frame:input:text" content="Enter your goal end date (dd/mm/yyyy)" />
       <meta property="fc:frame:button:1" content="Next" />
-      <meta property="fc:frame:post_url:2" content="${fullBaseUrl}/api/step3" />
+      <meta property="fc:frame:post_url:2" content="/api/step3" />
     </head>
     <body>
       <h1>Enter Your Goal End Date</h1>
