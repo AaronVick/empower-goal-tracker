fabb46ba02208d9aaec25d939dcbe130b6f449e4
Aaron <33428648+AaronVick@users.noreply.github.com>
2024-08-29 20:41:11 -0500

    update

--- scripts/sendCast.js
+++ scripts/sendCast.js
@@ -50,29 +50,35 @@ async function sendCast() {
         // Fetch the display name and custody address from Pinata API
         const response = await axios.get(`https://api.pinata.cloud/v3/farcaster/users/${fid}`, {
           headers: {
-            'Authorization': `Bearer ${process.env.PINATA_JWT}`
+            'Authorization': `Bearer ${process.env.PINATA_JWT}`,
+            'Content-Type': 'application/json'
           }
         });
 
         const displayName = response.data.user.display_name;
         const custodyAddress = response.data.user.custody_address;
+
         console.log('Display name found:', displayName);
         console.log('Custody address found:', custodyAddress);
 
-        // Construct the cast message with the custody address and other required fields
-        const message = {
-          castAddBody: {
-            text: `@${displayName} you're being supported on your goal, "${goalData.goal}", by ${goalData.supporters ? goalData.supporters.length : 0} supporters! Keep up the great work!\n\n${process.env.NEXT_PUBLIC_BASE_PATH}/goalShare?id=${doc.id}`,
-            embeds: [{ url: `${process.env.NEXT_PUBLIC_BASE_PATH}/goalShare?id=${doc.id}` }],
-            mentions: [fid],
-          },
-          signerId: custodyAddress
+        // Construct the cast message
+        const message = `@${displayName} you're being supported on your goal, "${goalData.goal}", by ${goalData.supporters ? goalData.supporters.length : 0} supporters! Keep up the great work!\n\n${process.env.NEXT_PUBLIC_BASE_PATH}/goalShare?id=${doc.id}`;
+
+        const castAddBody = {
+          text: message,
+          embeds: [{ url: `${process.env.NEXT_PUBLIC_BASE_PATH}/goalShare?id=${doc.id}` }],
+          mentions: [fid]
         };
 
+        const requestBody = JSON.stringify({
+          castAddBody: castAddBody,
+          signerId: custodyAddress,
+        });
+
         // Send the cast via the Farcaster API
-        const castResponse = await axios.post('https://api.pinata.cloud/v3/farcaster/casts', message, {
+        const castResponse = await axios.post('https://api.pinata.cloud/v3/farcaster/casts', requestBody, {
           headers: {
-            'Authorization': `Bearer ${process.env.WARPCAST_PRIVATE_KEY}`,
+            'Authorization': `Bearer ${process.env.PINATA_JWT}`,
             'Content-Type': 'application/json'
           }
         });
